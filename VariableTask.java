ef1ae79d430b096ce577bde1b9e7307ee1235203
Renuka2114 <145091982+Renuka2114@users.noreply.github.com>
2024-12-16 17:06:07 +0530

    day 1 task 2

---
+++ VariableTask.java
@@ -0,0 +1,33 @@
+class VariableTask{
+public static void main (String[] a){
+ 
+
+int i= 21;
+System.out.print("integer:" + i);
+
+byte b= 123;
+System.out.print("btye:" +b);
+
+short h= 14;
+System.out.print("short:"+h);
+
+long l= 9821125671l;
+System.out.print("long:" +l);
+
+float f= 21.14f;
+System.out.print("float:" +f);
+
+double d= 28.21141d;
+System.out.print("double:" +d);
+
+char c='R';
+System.out.print("char:" +c);
+
+boolean o= true;
+System.out.print("boolean:" +b);
+
+String s="Renu";
+System.out.print("String:" +s);
+}
+}
+
