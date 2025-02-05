813b7aa2701e8966d5b6ee598d2028f0d32be79e
Renuka2114 <145091982+Renuka2114@users.noreply.github.com>
2025-02-05 16:07:04 +0530

    Wrapper (auto and unboxing)

---
+++ AutoUnBoxing.java
@@ -0,0 +1,56 @@
+package Wrapper;
+
+public class AutoUnBoxing {
+	public static void main(String[] args) {
+		//int autoboxing and unboxing 
+		int a=21;
+		Integer a1=a;
+		int a2=a1;
+		System.out.println(a2);
+		
+		//byte
+		byte b=90;
+		Byte b1=b;
+		byte b2=b1;
+		System.out.println(b2);
+		
+		//boolean
+		boolean c=true;
+		Boolean c1=c;
+		boolean c2=c1;
+		System.out.println(c2);
+		
+		//char
+		char d='r';
+		Character d1=d;
+		char d2=d1;
+		System.out.println(d2);
+		
+		//double
+		double e=12.21;
+		Double e1=e;
+		double e2=e1;
+		System.out.println(e2);
+		
+		//float
+		float f=21.2f;
+		Float f1=f;
+		float f2=f1;
+		System.out.println(f2);
+		
+		//long
+		long g=246801237;
+		Long g1=g;
+		long g2=g1;
+		System.out.println(g2);
+		
+		//short
+		short h=2114;
+		Short h1=h;
+		short h2=h1;
+		System.out.println(h2);
+		
+		
+	}
+
+}
